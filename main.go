dce314a26174460c7d0b55ac2825a846b4bd23ff
一仝 <chun5060@outlook.com>
2024-12-21 19:58:36 +0800

    update

--- main.go
+++ main.go
@@ -26,7 +26,17 @@ func main() {
 	b, _ := json.Marshal(kb)
 	tv, _ := json.Marshal(t)
 	// markdown模板生成
-	_md := md.TempGenerator().H1("h1").NewLine().Image("desc", "url", 786, 343).Url("urltext", "urllink").Bold("boldtext").DividerLine().Code("code").Italic("italic")
+	_md := md.TempGenerator().
+		H1("h1").
+		NewLine().
+		Image("desc", "url", 786, 343).
+		Url("urltext", "urllink").
+		Bold("boldtext").
+		DividerLine().
+		Code("code").
+		Italic("italic")
+		// 原生markdown消息生成
+	m := md.Builder().CmdEnter("测试").CmdInput("测试", "显示", "true")
 	// 生成结果打印到控制台
-	fmt.Println(string(b), "\n\n\n", string(tv), "\n\n\n", _md.Str)
+	fmt.Println(string(b), "\n\n\n", string(tv), "\n\n\n", m.Str, "\n\n\n", _md.Str)
 }
--- markdown/markdown.go
+++ markdown/markdown.go
@@ -2,7 +2,6 @@ package markdown
 
 import (
 	"fmt"
-	"net/url"
 	"strings"
 )
 
@@ -151,7 +150,7 @@ func Builder() *MarkDown {
 	}
 }
 
-func (r *MarkDown) MqqApi(content string) *MarkDown {
+/* func (r *MarkDown) MqqApi(content string) *MarkDown {
 	str := fmt.Sprintf("\\n[%s](mqqapi://aio/inlinecmd?command=%s&reply=false&enter=false)", content, url.PathEscape(content))
 	r.Str += str
 	return r
@@ -174,6 +173,21 @@ func (r *MarkDown) MqqApiAt(nickname string, tinyId uint64) *MarkDown {
 func (r *MarkDown) MqqApiAtToProfile(nickname string, tinyId uint64) *MarkDown {
 	str := fmt.Sprintf("\\n[@%s](mqqapi://card/show_pslcard?src_type=internal&version=1&uin=%v&crad_type=friend&source=qrcode)", nickname, tinyId)
 	r.Str = str
+	return r
+} */
+
+// 客户端展示为： /回车指令 用户可点击的标签，群聊和文字子频道不支持该能力。
+func (r *MarkDown) CmdEnter(text string) *MarkDown {
+	str := fmt.Sprintf("<qqbot-cmd-enter text=\"%s\" />", text)
+	r.Str += str
+	return r
+}
+
+// 客户端展示为： /参数指令 用户可点击的标签
+// isTrue 为 true 或 false
+func (r *MarkDown) CmdInput(text, show, isTrue string) *MarkDown {
+	str := fmt.Sprintf("<qqbot-cmd-input text=\"%s\" show=\"%s\" reference\"%s\" />", text, show, isTrue)
+	r.Str += str
 	return r
 }
 
